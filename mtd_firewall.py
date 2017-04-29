5954d31f4f2b442d3ccc43abe609fd2540b2dadb
blueMac <bluebluebluelan@gmail.com>
2017-04-30 08:55:10 +0900

    unify get_json_method

--- mtd_firewall.py
+++ mtd_firewall.py
@@ -28,8 +28,31 @@ flow1 = {
     "eth_type":"0x0800",
     "ip_proto":"0x01",
     "ipv4_src":"10.0.0.1",
-    "eth_src":"12:bd:fe:73:00:0f",
     "icmpv4_type":"8",
+    "active":"true",
+    "actions":"output=4"
+}
+flow3 = {
+    'switch':str(switch_protect),
+    "name":"udp_beacon",
+    "cookie":"0",
+    "priority":"2",
+    "in_port":"5",
+    "eth_type":"0x0800",
+    "ip_proto":"0x11",
+    "ipv4_src":"10.0.0.1",
+    "active":"true",
+    "actions":"output=4"
+}
+flow4 = {
+    'switch':str(switch_protect),
+    "name":"tcp_beacon",
+    "cookie":"0",
+    "priority":"2",
+    "in_port":"5",
+    "eth_type":"0x0800",
+    "ip_proto":"0x06",
+    "ipv4_src":"10.0.0.1",
     "active":"true",
     "actions":"output=4"
 }
@@ -43,8 +66,32 @@ flow2 = {
     "eth_type":"0x0800",
     "ip_proto":"0x01",
     "ipv4_src":"10.0.0.1",
-    "eth_src":"12:bd:fe:73:00:0f",
+   # "eth_src":"12:bd:fe:73:00:0f",
     "icmpv4_type":"8",
+    "active":"true",
+    "actions":"output=1,set_field=eth_dst->86:ee:8f:9b:8d:a8,set_field=ipv4_dst->10.0.0.5"
+}
+flow5 = {
+    'switch':str(switch_protect),
+    "name":"tcp_block",
+    "cookie":"0",
+    "priority":"2",
+    "in_port":"5",
+    "eth_type":"0x0800",
+    "ip_proto":"0x11",
+    "ipv4_src":"10.0.0.1",
+    "active":"true",
+    "actions":"output=1,set_field=eth_dst->86:ee:8f:9b:8d:a8,set_field=ipv4_dst->10.0.0.5"
+}
+flow6 = {
+    'switch':str(switch_protect),
+    "name":"udp_block",
+    "cookie":"0",
+    "priority":"2",
+    "in_port":"5",
+    "eth_type":"0x0800",
+    "ip_proto":"0x06",
+    "ipv4_src":"10.0.0.1",
     "active":"true",
     "actions":"output=1,set_field=eth_dst->86:ee:8f:9b:8d:a8,set_field=ipv4_dst->10.0.0.5"
 }
@@ -91,8 +138,8 @@ pusher = StaticFlowPusher(controller_ip)
 #http://192.168.56.102:8080/wm/staticflowpusher/list/00:00:00:00:00:00:00:03/json
 #http://192.168.56.102:8080/wm/core/staticflowpusher/list/00:00:00:00:00:00:00:03/flow/json
 #http://192.168.56.102:8080/wm/core/switch/00:00:00:00:00:00:00:03/flow/json
-def get_icmp_echo_count():
-    url_json = "http://"+controller_ip+":8080/wm/core/switch/"+switch_protect+"/flow/json"
+def get_device_url(url_json):
+    #url_json = "http://"+controller_ip+":8080/wm/device/"
     #print url_json
     response = urllib2.urlopen(url_json)
     html = response.read()
@@ -101,19 +148,8 @@ def get_icmp_echo_count():
     response.close()
     return jsondata
 
-def push_icmp_beacon():
-    pusher.set(flow1)
-
-
-def get_protect_switch_flow():
-    url_json = "http://"+controller_ip+":8080/wm/core/switch/"+switch_protect+"/flow/json"
-    #print url_json
-    response = urllib2.urlopen(url_json)
-    html = response.read()
-    # parse response as json
-    jsondata = json.loads(html)
-    response.close()
-    return jsondata
+def push_icmp_beacon(flow):
+    pusher.set(flow)
 
 def statDaemon(d):
     #print switch_protect
@@ -124,13 +160,13 @@ def statDaemon(d):
                 flowPair[fPKey]=[]
                 flowPair[fPKey].append(int(d['packetCount']))
                 flowPair[fPKey].append(int(d['match']['in_port']))
-            #print d['packetCount']+"\n"
+            print d['packetCount']+"\n"
             flowPair[fPKey][0] = int(d['packetCount'])
             #print "haha"+str(data)
             flowSwid[switch_protect] = flowPair
     except:
         pass
-
+"""
 def combineFlowEntry_icmpblock(in_port, switch_protect):
     sss  = '{"switch":"'+str(switch_protect)+'", '
     sss += '"name":"block_icmp_'+str(flowCount)+'", '
@@ -148,7 +184,7 @@ def combineFlowEntry_icmpRedirect(in_port, switch_protect):
 
     return sss
 #sss = '{"switch":"00:00:00:00:00:00:00:02", "name":"block_icmp_01", "cookie":"0", "priority":"32768", "in_port":"3", "icmpv4_type":"8", "active":"true"}'
-
+"""
 #curl -X POST http://192.168.1.68:8080/wm/statistics/config/enable/json
 #curl -X GET http://192.168.1.68:8080/wm/statistics/bandwidth/00:00:00:00:00:00:00:02/3/json
 #curl -X GET http://192.168.1.68:8080/wm/core/switch/all/flow/json
@@ -181,10 +217,19 @@ curl -X POST -d '{
 #curl -X DELETE -d '{"name":"block_icmp_01"}' http://192.168.56.102:8080/wm/topology/route/00:00:00:00:00:00:00:01/1/00:00:00:00:00:00:00:02/2/json
 """
 initflag = True
+devicePair=[]
+url_json = "http://"+controller_ip+":8080/wm/device/"
+jsondata = get_device_url(url_json)
+for p in jsondata:
+    devicePair.append([p['mac'],p['ipv4'],0])
+print devicePair
+
+
 while 1:
 
     try:
-        jsondata = get_protect_switch_flow()
+        url_json = "http://"+controller_ip+":8080/wm/core/switch/"+switch_protect+"/flow/json"
+        jsondata = get_device_url(url_json)
 
         for data in jsondata:
             #print data
@@ -194,9 +239,12 @@ while 1:
         print flowSwid#[switch_protect]
     except:
         pass
-    push_icmp_beacon()
+    push_icmp_beacon(flow1)
+    push_icmp_beacon(flow3)
+    push_icmp_beacon(flow4)
 
-    jsondata = get_icmp_echo_count()
+    url_json = "http://"+controller_ip+":8080/wm/core/switch/"+switch_protect+"/flow/json"
+    jsondata = get_device_url(url_json)
 
    # print jsondata
     for i in jsondata["flows"]:
